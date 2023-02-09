9eb3495abf85e9db531fefb707f3f7c6de0c683d
zhoukun <mmqmzk@gmail.com>
2023-02-09 22:52:05 +0800

    Project structure

---
+++ utils-basic/src/main/java/me/zhoukun/Main.java
@@ -0,0 +1,11 @@
+package me.zhoukun;
+
+/**
+ * @author 周锟
+ * @date $DATE $TIME
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
